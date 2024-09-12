d2457a372c605b35e19ed955ae77176124945221
Victor de Souza Xavier Fernandes <104697605+victorsxavierf@users.noreply.github.com>
2024-09-11 21:06:13 -0300

    Update questao4.py

---
+++ questao4.py
@@ -0,0 +1,8 @@
+"""
+a) 9 Impares
+b) 128 Dobro do último número
+c) 49 O quadrado do número da posição
+d) 100 O quadrado de número pares
+e) 13 Sequência de Fibonnaci
+f) 1 Sequência sendo de intervalos de 8,2,4,1,1,1,1...
+"""
