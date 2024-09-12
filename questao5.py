c2415ee1a2e709113bb104eb91ce76bdf9d9eb30
Victor de Souza Xavier Fernandes <104697605+victorsxavierf@users.noreply.github.com>
2024-09-11 21:06:58 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,7 +1,13 @@
-# Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+""" Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. 
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, 
+qual interruptor controla cada lâmpada?"""
 
 """
 Três interruptores, cada um conectado a uma lâmpada diferente.
 
-Primeiramente eu iria ligar o interruptor do meio e descobrir qual lâmpada foi acesa, logo posteriormente iria ligar qualquer um dos dois interruptores restantes. Deixaria ligado por 2 minutos e voltaria para a sala, a lâmpada que estivesse um pouco quente foi do interruptor que havia ligado. Deixando a última lâmpada sendo do interruptor que sobrou.
+Primeiramente eu iria ligar o interruptor do meio e descobrir qual lâmpada foi acesa, 
+logo posteriormente iria ligar qualquer um dos dois interruptores restantes. 
+Deixaria ligado por 2 minutos e voltaria para a sala, a lâmpada que estivesse um pouco quente foi do interruptor que havia ligado. 
+Deixando a última lâmpada sendo do interruptor que sobrou.
 """
