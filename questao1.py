0ff07cbd26d9014c33f424a3c6080e807aeb5b59
Victor de Souza Xavier Fernandes <104697605+victorsxavierf@users.noreply.github.com>
2024-09-11 21:05:22 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -15,9 +15,9 @@ def main():
     try:
         numero = int(input("Informe um número para verificar se pertence à sequência de Fibonacci: "))
         if pertence_fibonacci(numero):
-            print(f"O número {numero} pertence à sequência de Fibonacci.")
+            print("O número " ,numero, " pertence à sequência de Fibonacci.")
         else:
-            print(f"O número {numero} não pertence à sequência de Fibonacci.")
+            print("O número " ,numero, " não pertence à sequência de Fibonacci.")
     except ValueError:
         print("Por favor, informe um número válido.")
 
