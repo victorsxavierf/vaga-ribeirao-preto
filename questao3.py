b4c604b49fa65f78eeb239b4c2ae860a9d12386e
Victor de Souza Xavier Fernandes <104697605+victorsxavierf@users.noreply.github.com>
2024-09-11 21:05:53 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,5 +1,5 @@
 # Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
-
+# O resultado da soma é 77
 indice = 12
 soma = 0
 k = 1
